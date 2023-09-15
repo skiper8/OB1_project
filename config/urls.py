c77bfee027b01d9934846bef6a4efe2fc28a52ff
askip <a.skiper2013@yandex.ru>
2023-09-15 21:22:05 +0300

    Добаление комментариев

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,8 +36,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('school.urls', namespace='school')),
-    path('users/', include('users.urls', namespace='users')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+                  path('admin/', admin.site.urls),
+
+                  # School
+                  path('', include('school.urls', namespace='school')),
+
+                  # Users
+                  path('users/', include('users.urls', namespace='users')),
+
+                  # Swagger
+                  path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
