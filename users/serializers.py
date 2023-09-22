17f036150a1106b422e46c3f652c8b19d52d96e4
askip <a.skiper2013@yandex.ru>
2023-09-22 11:07:25 +0300

    Исправление serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -13,11 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, min_length=6, max_length=12)
+    password = serializers.CharField(write_only=True, required=True, min_length=2, max_length=40)
 
     class Meta:
         model = User
         fields = ['id', 'email', 'password']
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name']
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
